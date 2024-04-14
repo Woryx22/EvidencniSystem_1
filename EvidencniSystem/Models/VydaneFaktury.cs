774f7733f8fc9e3db57046b82703833c9044714a
Lukáš Lešák <lukas.woryx@gmail.com>
2024-04-14 11:31:42 +0200

    Vydane faktury

--- EvidencniSystem/Models/VydaneFaktury.cs
+++ EvidencniSystem/Models/VydaneFaktury.cs
@@ -1,14 +1,26 @@
-﻿namespace EvidencniSystem.Models
+﻿
+
+namespace EvidencniSystem.Models
 {
     public class VydaneFaktury
     {
         public int Id { get; set; }
 
-        public string Name { get; set; }
+        public string Odberatel { get; set; }
+
+        public string CisloObjednavky { get; set; }
+
+        public string Popis { get; set; }
+
+        public string Vystaveno { get; set; }
+
+        public string Splatnost { get; set; }
+
+        public string ZpusobUhrady { get; set; }
 
         public override string ToString()
         {
-            return $"{Name}";
+            return $"{Odberatel} {CisloObjednavky} {Popis} {Vystaveno} {Splatnost}  {ZpusobUhrady} " ;
         }
     }
 }
--- EvidencniSystem/VydaneFaktury_Page.xaml.cs
+++ EvidencniSystem/VydaneFaktury_Page.xaml.cs
@@ -14,20 +14,37 @@ public partial class VydaneFaktury_Page : ContentPage
         _context = new();
         InitializeComponent();
         lst2.ItemsSource = _context.VydaneFaktury_.ToList(); // pøipojení zdroje dat k ListView
-        picker.ItemsSource = _context.Odberatele.ToList();
+        forOdberatel.ItemsSource = _context.Odberatele.ToList();
     }
 
     private void SaveVydaneFaktury(object sender, EventArgs e)
     {
-        VydaneFaktury newVydaneFaktury = new()
-        {
-            Name = forName.Text
-        };
+        // Získání vybraného objektu typu Odberatel
+        Odberatel selectedOdberatel = forOdberatel.SelectedItem as Odberatel;
 
-        _context.Add(newVydaneFaktury); // pøidá záznam do Data Setu
-        _context.SaveChanges(); // uloží zmìny do databáze !!!!!!
-        refresh();
+        // Pokud je vybrán nìjaký objekt
+        if (selectedOdberatel != null)
+        {
+            VydaneFaktury newVydaneFaktury = new VydaneFaktury
+            {
+                Odberatel = selectedOdberatel.Name,
+                CisloObjednavky = forCisloObjednavky.Text,
+                Popis = forPopis.Text,
+                Vystaveno = forVystaveno.Date.ToShortDateString(),
+                Splatnost = forSplatnost.Date.ToShortDateString(),
+                ZpusobUhrady = (string)forZpusobUhrady.SelectedItem
+            };
+
+            _context.Add(newVydaneFaktury);
+            _context.SaveChanges();
+            refresh();
+        }
+        else
+        {
+            //bleeh
+        }
     }
+
 
     private void Smazat(object sender, EventArgs e)
     {
